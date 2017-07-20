e6ac8be1fd82c580c4c3e8bd2c17712cec1a85ad
Jatinder Singh <jkhabra09@gmail.com>
2017-07-20 14:04:11 +0530

    Add image_compression function

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from css_html_js_minify import css_minify, html_minify
 from jinja2 import Environment, FileSystemLoader
+from PIL import Image
 import os
 
 cwd = os.path.dirname(os.path.abspath(__file__))
@@ -20,7 +21,7 @@ def compile_template(template, name):
 
 def minify_css():
     if not os.path.exists(bundle_dir):
-        dir = os.path.dirname(bundle_path)
+        dir = os.path.dirname(bundle_dir)
         os.makedirs(dir)
         print('Directory: {} created'.format(dir))
 
@@ -42,6 +43,29 @@ def minify_html():
                 with open(i, 'w') as html_min_file:
                     min = html_min_file.write(min_html)
 
+def jpg_into_png(image):
+    images_path = os.listdir(static_dir + 'images/')
+
+    for i in images_path:
+        if i.endswith('.jpg'):
+            image = Image.open(static_dir+'images/'+i)
+            print('converting {} into png'.format(i))
+            image.convert('RGB').save(bundle_dir + '{}{}.png'.format('images/', i.split('.')[0]), 'PNG')
+
+def image_compression():
+    if not os.path.exists(bundle_dir + 'images/'):
+        dir = os.path.dirname(bundle_dir + 'images/')
+        os.makedirs(dir)
+
+    images_path = os.listdir(static_dir + 'images/')
+    #images_path = os.listdir(bundle_dir + 'images/')
+
+    for i in images_path:
+        if i.endswith('.jpg'):
+            image = Image.open(static_dir + 'images/' + i)
+            #image_sieze = image.size
+            print('compressing {} '.format(i))
+            image.save(bundle_dir + 'images/{}'.format(i),optimize=True,quality=75)
 
 if __name__ == '__main__':
     print('Compiling index and work templates')
